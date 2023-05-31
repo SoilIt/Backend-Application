83c1cbdf4d04a3937a5f2b2713a5daf9cf8d5d40
Adiva Veronia <85977959+adivaveronia@users.noreply.github.com>
2023-05-31 18:42:29 +0700

    Commit Rest API for campaign and latest history data

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const Firestore = require('@google-cloud/firestore')
+const db = new Firestore();
+const app = express();
+app.use(express.json());
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`SoilIt Rest API listening on port ${port}`);
+});
+
+app.get('/', async (req, res) => {
+    res.send('SoilIt is ready!');
+})
+
+// Home page
+// Get latest soil detection history
+app.get('/history/latest/{user_id}', async (req, res) => {
+    try {
+        const user_id = req.params.user_id;
+        const query = db.collection('').where('user_id', '==', user_id).orderBy('timestamp', 'desc').limit(1);
+        const querySnapshot = await query.get();
+        // const data = querySnapshot.docs.map(doc => doc.data());
+        if (querySnapshot.size > 0) {
+            res.json(querySnapshot.docs[0].data());
+            res.status(200);
+        }
+        else {
+            res.json({status: 'Not found'});
+        }
+    } catch (error) {
+        res.status(500).send(error);
+    }
+    
+})
+// Get 5 campaign data
+app.get('/campaign/limit', async (req, res) => {
+    try {
+        const query = db.collection('').orderBy('timestamp', 'desc').limit(5);
+        const querySnapshot = await query.get();
+        const data = querySnapshot.docs.map(doc => doc.data());
+        res.json(data); 
+        res.status(200);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+})
