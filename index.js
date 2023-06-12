15eb62ad5062ac37da11def224c014c4022a725f
Hany Yesica <77098541+hanscaz@users.noreply.github.com>
2023-06-12 13:33:47 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,112 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const Firestore = require('@google-cloud/firestore')
+const db = new Firestore();
+const app = express();
+app.use(express.json());
+const port = process.env.PORT || 9000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.listen(port, function (err) {
+    if (err) console.log(err);
+    console.log(`SoilIt Rest API listening on port ${port}`);
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+//GET list campaign
+app.get('/campaign', async (req, res) =>{
+        const query = db.collection('campaign').orderBy('created_at')
+        const querySnapshot = await query.get()
+        let dataArr = [];
+        querySnapshot.forEach(result=> {
+        dataArr.push(result.data())
+        })
+        
+        console.log(`querySnapshot size = ${querySnapshot.size}`)
+        if (querySnapshot.size > 0) {
+            res.send(dataArr)
+            res.status(200)
+        }
+        else {
+            res.jsonp({
+              message:  "Can not found"
+            })
+         }
+  }
+  )
+
+//GET list history
+app.get('/history', async (req, res) =>{
+        const query = db.collection('history').orderBy('created_at')
+        const querySnapshot = await query.get()
+        let dataArr = [];
+        querySnapshot.forEach(result=> {
+        dataArr.push(result.data())
+        })
+        
+        console.log(`querySnapshot size = ${querySnapshot.size}`)
+        if (querySnapshot.size > 0) {
+            res.send(dataArr)
+            res.status(200)
+        }
+        else {
+            res.jsonp({
+              message:  "Can not found"
+            })
+         }
+  }
+  )
+
+// list FAQ
+app.get('/faq', async (req, res) =>{
+    const query = db.collection('FAQ');
+    const querySnapshot = await query.get();
+    let dataArr = [];
+        querySnapshot.forEach(result=> {
+        dataArr.push(result.data())
+        })
+        
+        console.log(`querySnapshot size = ${querySnapshot.size}`)
+        if (querySnapshot.size > 0) {
+            res.send(dataArr)
+            res.status(200)
+        }
+        else {
+            res.jsonp({
+              message:  "Can not found"
+            })
+         }
+  }
+  )
+
+//GET detail history
+app.get('/history/:id', async (req, res) =>{
+    const id = req.params.id;
+    const query = db.collection('history').where('id', '==', id);
+    const querySnapshot = await query.get();
+    let dataArr = [];
+        querySnapshot.forEach(result=> {
+        dataArr.push(result.data())
+        })
+        
+        console.log(`querySnapshot size = ${querySnapshot.size}`)
+        if (querySnapshot.size > 0) {
+            res.send(dataArr)
+            res.status(200)
+        }
+        else {
+            res.jsonp({
+              message:  "Can not found"
+            })
+         }
+  }
+  )
+
+//DELETE history
+app.delete('/history/:id', async (req, res) => {
+    
+    const id = req.params.id;
+    const query = db.collection('history').where('id', '==', id);
+    const querySnapshot = await query.delete();
+    res.status(200).json ({
+        message: 'history deleted successfuly'
+    })
+    
 })
